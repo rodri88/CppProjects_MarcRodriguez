7c99f9391ca37a7292db8b5fc753629b62d28371
mrodriguez <mrodriguez@S3-43>
2017-02-17 14:00:49 +0100

    17/02/17

--- RandomEnemyGenerator/RandomEnemyGenerator.cpp
+++ RandomEnemyGenerator/RandomEnemyGenerator.cpp
@@ -2,14 +2,23 @@
 #include <string>
 #include <ctime>
 
+//using namespace std;
+
 enum class EnemyType {zombie, vampire, ghost, witch};
 
 struct Enemy {
-	//EnemyType;
-
-
+	EnemyType type;
+	std::string name;
+	int health;
 };
+
+/*Enemy CreateRandomEnemy() {
+	Enemy->type = rand();
+
+	return {}
+}*/
 
 int main() {
 
+	return 0;
 }
